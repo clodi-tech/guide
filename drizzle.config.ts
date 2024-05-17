4013e385cbbc8c7ec5c8cef21eb0e03ae421d3fd
clodi <ciao@clodi.tech>
2024-05-17 11:40:54 +0200

    reset

--- drizzle.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from "drizzle-kit";
-export default {
-  schema: "./db/schema.ts",
-  out: "./db/migrations",
-  driver: "pg",
-  dbCredentials: {
-    connectionString: process.env.AUTH_DRIZZLE_URL!,
-  },
-} satisfies Config;
