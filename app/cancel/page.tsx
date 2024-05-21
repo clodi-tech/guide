d823ca44d60b393d4b5e761de0689c023cb1a073
clodi <ciao@clodi.tech>
2024-05-21 11:34:46 +0200

    fix build error

--- app/cancel/page.tsx
+++ app/cancel/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
     <div>
-      <h1>I'm sorry you canceled!</h1>
+      <h1>I am sorry you canceled!</h1>
       <p>Here is a 10% discount: CANCEL10</p>
     </div>
   );
