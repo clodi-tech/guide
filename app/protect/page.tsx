e8e96f713c860fd72614a13b90a3edfee74ed80c
clodi <ciao@clodi.tech>
2024-05-21 10:56:49 +0200

    fix render protect

--- app/protect/page.tsx
+++ app/protect/page.tsx
@@ -4,11 +4,13 @@ import { SignInButton } from "@/components/signin";
 
 export default async function Page() {
   const session = await auth();
-  if (!session?.user) return;
-  <div>
-    <h1>Protected Page</h1>
-    <SignInButton />
-  </div>;
+  if (!session?.user)
+    return (
+      <div>
+        <h1>Protected Page</h1>
+        <SignInButton />
+      </div>
+    );
 
   return (
     <div>
